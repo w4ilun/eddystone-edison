52309ea6cb48d5e9d90832403f643d412a673c9c
w4ilun <w4ilun@yahoo.com>
2016-05-03 17:49:36 -0400

    demo script

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-Hello World
+navigator.bluetooth.requestDevice({
+  filters: [{ name: 'Edison' }],
+  optionalServices: [0xFC00]
+});
+
+.then(device => device.gatt.connect());
+
+.then(server => server.getPrimaryService(0xFC00));
+
+.then(service => {
+  let p1 = () => service.getCharacteristic(0xFC0B)
+  .then(characteristic => {
+   	characteristic.writeValue(0); 
+   });
+
+
+
+  return p1();
+})
+
+.catch(err => {
+  // Catch any error.
+})
+          
+.then(() => {
+  // Connection fully established, unless there was an error above.
+});
