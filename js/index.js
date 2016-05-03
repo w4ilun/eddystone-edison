2d508a7e078ac55e857b2840bb547c4ee9776578
w4ilun <w4ilun@yahoo.com>
2016-05-03 17:58:36 -0400

    m

--- js/index.js
+++ js/index.js
@@ -1,11 +1,19 @@
+(function(){
+
 navigator.bluetooth.requestDevice({
   filters: [{ name: 'Edison' }],
   optionalServices: [0xFC00]
 })
 
-.then(device => device.gatt.connect())
+.then(device => {
+    alert("Connecting to GATT server...");
+    return device.gatt ? device.gatt.connect() : device.connectGATT();
+})
 
-.then(server => server.getPrimaryService(0xFC00))
+.then(server => {
+	alert("Server Connected!");
+	server.getPrimaryService(0xFC00);
+})
 
 .then(service => {
   let p1 = () => service.getCharacteristic(0xFC0B)
@@ -25,3 +33,5 @@ navigator.bluetooth.requestDevice({
 .then(() => {
   // Connection fully established, unless there was an error above.
 });
+
+})();
